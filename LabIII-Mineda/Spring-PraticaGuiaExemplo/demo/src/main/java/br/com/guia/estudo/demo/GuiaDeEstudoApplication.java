12e7017579b03b0d23fc5a2647ea98160a5a8ce2
Elbert Jean <elbertjeandossantos@gmail.com>
2025-04-03 19:36:42 -0300

    :tada: init: inicializacao estudo guia springboot

---
+++ LabIII-Mineda/Spring-PraticaGuiaExemplo/demo/src/main/java/br/com/guia/estudo/demo/GuiaDeEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guia.estudo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaDeEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaDeEstudoApplication.class, args);
+	}
+
+}
---
+++ LabIII-Mineda/Spring-PraticaGuiaExemplo/demo/src/test/java/br/com/guia/estudo/demo/GuiaDeEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guia.estudo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaDeEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
